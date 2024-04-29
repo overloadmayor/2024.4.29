2bd6303d711432883169672c694b4efe5865962b
qianfan <nhzx118wrk@163.com>
2024-04-29 19:43:42 +0800

    添加了异步Promise

--- typescript/mayor.ts
+++ typescript/mayor.ts
@@ -1,2 +1,15 @@
 console.log("Niubi mayor")
 let a = 10
+interface resp {
+    status: number,
+    message: string
+    data: any
+}
+const fetchData = async (url: string): Promise<resp> => {
+    return Promise.resolve({
+        status: 1,
+        message: "string",
+        data: "any"
+    })
+
+}
